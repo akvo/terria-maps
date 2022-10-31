ea2522b9818977412298676a451dbf5280a70560
dedenbangkit <mail@dedenbangkit.com>
2022-10-31 10:36:23 +0700

    [#1] Deploy script, update release.sh

--- frontend/release.sh
+++ frontend/release.sh
@@ -3,3 +3,12 @@
 
 yarn install
 yarn gulp release
+
+ls -al
+
+rm wwwroot/config.json
+cp /etc/config/client/config.json wwwwroot/config.json
+cp ./index.js wwwroot/index.js
+cp ./version.js wwwroot/version.js
+
+ls -al
