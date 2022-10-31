9863ab151def72f2239f5f28eb12bf110f87b3b8
dedenbangkit <mail@dedenbangkit.com>
2022-10-31 09:49:29 +0700

    [#1] Nginx config

--- ci/build.sh
+++ ci/build.sh
@@ -5,6 +5,19 @@ set -exuo pipefail
 [[ "${CI_BRANCH}" ==  "gh-pages" ]] && { echo "GH Pages update. Skip all"; exit 0; }
 [[ -n "${CI_TAG:=}" ]] && { echo "Skip build"; exit 0; }
 
+## RESTORE IMAGE CACHE
+IMAGE_CACHE_LIST=$(grep image ./docker-compose.yml \
+    | sort -u | sed 's/image\://g' \
+    | sed 's/^ *//g')
+mkdir -p ./ci/images
+
+while IFS= read -r IMAGE_CACHE; do
+    IMAGE_CACHE_LOC="./ci/images/${IMAGE_CACHE//\//-}.tar"
+    if [ -f "${IMAGE_CACHE_LOC}" ]; then
+        docker load -i "${IMAGE_CACHE_LOC}"
+    fi
+done <<< "${IMAGE_CACHE_LIST}"
+
 image_prefix="eu.gcr.io/akvo-lumen/terria-maps"
 
 dc () {
