90784cfac7000de920f81498cc1d20c74fd48418
dedenbangkit <mail@dedenbangkit.com>
2022-10-31 13:47:25 +0700

    [#1] Fix server config

--- frontend/release.sh
+++ frontend/release.sh
@@ -9,7 +9,3 @@ ls -al
 #rm wwwroot/config.json
 #cp /etc/config/client/config.json wwwwroot/config.json
 cp /app/devserverconfig.json serverconfig.json
-export NODE_ENV=production
-node ./node_modules/terriajs-server/lib/app.js --config-file serverconfig.json
-
-ls -al
